f30bffb4fe7dca056971c13eccae75c47a300bf7
Christina Thiphavong <christinathip@gmail.com>
2020-01-22 11:15:32 -0600

    commit

--- static/js/app.js
+++ static/js/app.js
@@ -3,27 +3,61 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
+  d3.json(`/metadata/${sample}`).then((data) => {
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    var panel = d3.select(`#sample-metadata`);
     // Use `.html("") to clear any existing metadata
-
+    panel.html("");
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+    Object.entries(data).forEach(([key, value]) => {
+      panel.append("h6").text(`${key}:${value}`);
+    })
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+    buildGauge(data.WREQ);
+  })
 }
 
 function buildCharts(sample) {
-
+  const url = "/samples/" + sample;
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-
+  d3.json(url).then((samples) => {
     // @TODO: Build a Bubble Chart using the sample data
+    let otu_ids = samples["otu_ids"];
+    let sample_values = samples["sample_values"];
+    let otu_labels = samples["otu_labels"];
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+    let trace1 = {
+        labels: otu_ids.slice(0, 10),
+        values: sample_values.slice(0, 10),
+        hovertext: otu_labels.slice(0, 10),
+        type: "pie"
+    };
+
+    let data = [trace1];
+    Plotly.newPlot("pie", data);
+
+    let trace2 = {
+        x: otu_ids,
+        y: sample_values,
+        size: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+            size: sample_values,
+            opactiy: 0.3,
+            color: otu_ids
+        }
+    };
+    let bubble_data = [trace2];
+    Plotly.newPlot("bubble", bubble_data);
+  })
 }
 
 function init() {
